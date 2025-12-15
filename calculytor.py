15fcb75ce5ce76c4159cc9b26026a5170a0c6328
Sasha <aleksandravolkova1414@gmail.com>
2025-12-15 12:47:40 +0300

    Добавлены функции умножения и вычитания

--- calculytor.py
+++ calculytor.py
@@ -4,4 +4,8 @@ def divide(a, b):
     if b == 0: 
         raise ValueError("Деление на ноль невозможно") 
     return a / b 
+def multiply(a, b): 
+    return a * b 
+def subtract(a, b): 
+    return a - b 
 print(add(7.5, 2.5))
