350d84c195da84ede4d6d8d3c0f5eef3ebaf7e53
Sasha <aleksandravolkova1414@gmail.com>
2025-12-15 13:07:20 +0300

    Добавлена функция сравнения

--- calculytor.py
+++ calculytor.py
@@ -8,4 +8,11 @@ def multiply(a, b):
     return a * b 
 def subtract(a, b): 
     return a - b 
+def compare_numbers(a, b):
+    if a > b:
+        return f"{a} больше чем {b}"
+    elif a < b:
+        return f"{a} меньше чем {b}"
+    else:
+        return f"a} равно {b}"
 print(add(7.5, 2.5))
